33076694f3b5df6c59afbc5bd99b9f02488ecdae
Viktoria <123vika@gmail.com>
2020-01-30 08:18:59 +0200

    HomeWork_1_kotlin

--- app/src/main/java/com/example/kotlinhomework/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhomework/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.kotlinhomework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun onClick(view: View) {
         button.setOnClickListener {
             Toast.makeText(this, "Hello Kotlin!", Toast.LENGTH_SHORT).show()
         }
